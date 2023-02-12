3fbc03e6eb7c6618203c44566f4005a1bba7b4ed
luciano <lucianobenjaminpm@gmail.com>
2023-02-12 13:50:07 -0300

    Add models to polls and add app(polls) to settings

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Candidate(models.Model):
+    name = models.CharField(max_length=30)
+    party = models.CharField(max_length=20)
+    votes = models.IntegerField(null=False, default=0)
+    
+class Voter(models.Model):
+    name = models.CharField(max_length=30)
+    vote = models.ForeignKey(Candidate,on_delete=models.CASCADE)
